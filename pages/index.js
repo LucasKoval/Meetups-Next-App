98db1d1d32d3607bb448a7dfff6fe88714e00c9a
Lucas Koval <lucaskovaldev@gmail.com>
2021-12-12 22:19:29 -0300

    Project finished

--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,56 @@
+import Head from 'next/head'
+import { MongoClient } from 'mongodb'
 import MeetupList from '../components/meetups/MeetupList'
 
-const EXAMPLE_MEETUPS = [
-  {
-    id: 'm1',
-    title: 'A First Meetup',
-    image:
-      'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg',
-    address: 'Some address 5, 12345 Some City',
-    description: 'This is a first meetup!',
-  },
-  {
-    id: 'm2',
-    title: 'A Second Meetup',
-    image:
-      'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg',
-    address: 'Some address 10, 12345 Some City',
-    description: 'This is a second meetup!',
-  },
-]
-
-const HomePage = () => {
-  return <MeetupList meetups={EXAMPLE_MEETUPS} />
+const HomePage = (props) => {
+  return (
+    <>
+      <Head>
+        <title>NextJS Meetups App</title>
+        <meta name="description" content="Browse a list of highly active React meetups!" />
+      </Head>
+      <MeetupList meetups={props.meetups} />
+    </>
+  )
 }
+
+// INCREMENTAL STATIC GENERATION APPROACH (SSG)
+export async function getStaticProps() {
+  // Fetch data from an API
+  const client = await MongoClient.connect(
+    'mongodb+srv://LucasKoval:103497610Xregmdb@cluster0.ddley.mongodb.net/meetups?retryWrites=true&w=majority'
+  )
+  const db = client.db()
+  const meetupsCollection = db.collection('meetups')
+
+  const meetups = await meetupsCollection.find().toArray()
+  client.close()
+
+  return {
+    props: {
+      meetups: meetups.map((meetup) => ({
+        title: meetup.title,
+        address: meetup.address,
+        image: meetup.image,
+        id: meetup._id.toString(),
+      })),
+    },
+    revalidate: 10, //--> This page will be re-pregenerated every 10 seconds after deployment (INCREMENTAL STATIC GENERATION)
+  }
+}
+
+/* SERVER-SIDE RENDER APPROACH (SSR)
+export async function getServerSideProps(context) {
+  const req = context.req
+  const res = context.res
+  // Fetch data from an API
+
+  return {
+    props: {
+      meetups: EXAMPLE_MEETUPS,
+    },
+  }
+}
+*/
 
 export default HomePage
--- pages/new-meetup/index.js
+++ pages/new-meetup/index.js
@@ -1,11 +1,32 @@
+// our-domain.com/new-meetup
+import Head from 'next/head'
+import { useRouter } from 'next/router'
 import NewMeetupForm from '../../components/meetups/NewMeetupForm'
 
 function NewMeetupPage() {
-  function addMeetupHandler(enteredMeetupData) {
-    console.log(enteredMeetupData)
+  const router = useRouter()
+
+  async function addMeetupHandler(enteredMeetupData) {
+    const response = await fetch('/api/new-meetup', {
+      method: 'POST',
+      body: JSON.stringify(enteredMeetupData),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+
+    const data = await response.json()
+    router.push('/')
   }
 
-  return <NewMeetupForm onAddMeetup={addMeetupHandler} />
+  return (
+    <>
+      <Head>
+        <title>Add a New Meetup</title>
+      </Head>
+      <NewMeetupForm onAddMeetup={addMeetupHandler} />
+    </>
+  )
 }
 
 export default NewMeetupPage
